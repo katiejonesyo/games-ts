a3a99a7acf3a47eeddd836f4c57c843447880cf7
Katelinn Jones <katiejonesyo@gmail.com>
2021-04-19 13:04:37 -0700

    cleaned up code in memory file, added mobile css

--- base/app.js
+++ base/app.js
@@ -1,1 +1,8 @@
 "use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const allSquares = [];
+    const scoreDisplay = document.querySelector('#current-score');
+    const resetButton = document.querySelector('#reset-button');
+});
+
